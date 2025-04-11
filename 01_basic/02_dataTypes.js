f7734a34645257255d562fc0a20bcd48e6361dd6
manishgupta77 <154451097+manishgupta77@users.noreply.github.com>
2025-04-11 18:19:12 +0000

    container config done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("manish")
+
+
+let name = "manish"
+let age = 18
+let isloggedIn = false
+let state;
+
+
+//number => 2 to power 53
+// bright
+//string => ""
+// boolean =. true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
