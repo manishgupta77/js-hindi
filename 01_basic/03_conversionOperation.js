abb46932f95a671bd074ad59e333a80b3e1c1d54
manishgupta77 <154451097+manishgupta77@users.noreply.github.com>
2025-04-12 18:35:51 +0000

    config done

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = null
+let score = "33ab"
 
 //console.log(typeof score);
 //console.log(typeof(score));
@@ -23,6 +23,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//*******************Operations *************
+
+let value = 3
+let negValue = -value
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+//console.log("1" + 2)
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log(1 + 2 + "2");
+
+//console.log( (3 + 4) * 5 % 3)
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//link to study
 
