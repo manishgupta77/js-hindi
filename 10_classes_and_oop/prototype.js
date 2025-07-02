c0a2663ef89b659089217549fb223d152dab07a0
manishgupta77 <154451097+manishgupta77@users.noreply.github.com>
2025-07-02 11:20:17 +0000

    prototype.js

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,28 @@
+// let myName = "manish"
+// let mychannel = "chai"
+
+// console.log(myName.trueLength);
+
+
+let myheros = ["thor", "spiderman"]
+
+let heropower = {
+   
+}
+
+Object.prototype.manish = function(){
+    console.log('manish is present in all object');
+}
+
+Array.prototype.heymanish = function(){
+    console.log('manish says hello');
+}
+
+// heroPower.Mansih()
+myheros.manish()
+myheros.heyManish()
+// heroPower.heyManish
+
+
+// Inheritance
+
