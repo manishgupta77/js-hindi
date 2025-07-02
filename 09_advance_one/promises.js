ee1116a3918505896de1bdc4b8876276688b6923
manishgupta77 <154451097+manishgupta77@users.noreply.github.com>
2025-07-02 09:18:29 +0000

    advance.cont

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -94,3 +94,15 @@ consumePromiseFive()
 } 
 getAllUsers()*/
 
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+
+})
+.then((data) => {
+    console.log(data);
+
+})
+.catch((error) => console.log(error))
+
