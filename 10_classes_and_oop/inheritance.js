8275f96e51e7d4d97bfc48610f83874b71ccdb24
manishgupta77 <154451097+manishgupta77@users.noreply.github.com>
2025-07-03 18:44:32 +0000

    done.contain

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,22 @@
+class USer {
+    constructor(username){
+        this.username = username
+    }
+
+    logme(){
+        console.log('USERNAME is ${this.username}');
+
+    }
+
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+    addCourse(){
+        console.log('A new course was added by ${this.username}');
+    }
+}
