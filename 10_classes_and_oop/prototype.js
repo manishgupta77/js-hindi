dd9c56f7d63eafbdcd49156528ceb988babbe4ed
manishgupta77 <154451097+manishgupta77@users.noreply.github.com>
2025-07-03 13:33:15 +0000

    done.cont

--- 10_classes_and_oop/prototype.js
+++ 10_classes_and_oop/prototype.js
@@ -19,10 +19,47 @@ Array.prototype.heyManish = function(){
 }
 
 // heroPower.Mansih()
-myheros.manish()
-myheros.heyManish()
+// myheros.manish()
+// myheros.heyManish()
 // heroPower.heyManish
 
 
 // Inheritance
 
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideos: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'js Assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode    "  
+
+String.prototype.trueLength = function(){
+    console.log('${this}');
+    // console.log('${this.name}');
+    console.log('True length is: ${this.trim().length}');
+
+}
+
+anotherUsername.trueLength()
+"manish".trueLength()
+"iceTea".truelength()
